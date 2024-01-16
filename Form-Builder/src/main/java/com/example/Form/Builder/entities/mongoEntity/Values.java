bce09c5696c5c64eb2f8bd7fc195531f8080f18e
deepak <deepakshaji83@gmail.com>
2024-01-16 16:10:01 +0530

    MongoEntity  updated

--- Form-Builder/src/main/java/com/example/Form/Builder/entities/mongoEntity/Values.java
+++
@@ -1,9 +0,0 @@
-package com.example.Form.Builder.entities.mongoEntity;
-
-import org.springframework.data.annotation.Id;
-
-public class Values {
-    @Id
-    private String valueId;
-    private String value;
-}
