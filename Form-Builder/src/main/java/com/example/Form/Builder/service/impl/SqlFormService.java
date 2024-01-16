c10a696c168c8dfe38e90a0ab2d7db4021f8e1a0
abhijithjana-001 <abhijith.kme19cs002@gmail.com>
2024-01-16 12:48:16 +0530

    sql service  crud

--- Form-Builder/src/main/java/com/example/Form/Builder/service/impl/SqlFormService.java
+++ Form-Builder/src/main/java/com/example/Form/Builder/service/impl/SqlFormService.java
@@ -4,6 +4,7 @@ import com.example.Form.Builder.dto.response.ResponseDto;
 import com.example.Form.Builder.entities.entity.Form;
 import com.example.Form.Builder.repostory.SqlRepo;
 import com.example.Form.Builder.service.FormService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -21,25 +22,23 @@ public class SqlFormService implements FormService {
 
     @Override
     public ResponseDto<List<Object>> saveOrUpdateForm(Form form, String title) {
-
-        if(title!=null){
-          form.setId(repo.findByTitle(title).get().getId());
-
-
+        if(title!=null) {
+            form.setId(repo.findByTitle(title).get().getId());
         }
         Form save = repo.save(form);
-
-        return new ResponseDto<>(true,"Form updated successfully",Arrays.asList(save));
-
+        return new ResponseDto<>(true,"Form created or updated successfully",Arrays.asList(save));
     }
 
     @Override
+    @Transactional
     public ResponseDto<Form> deleteFormByTitle(String title) {
-        return null;
+        repo.deleteByTitle(title);
+        return new ResponseDto<>(true,"Form deleted successfully",null);
     }
 
     @Override
     public ResponseDto<Form> getFormByTitle(String title) {
-        return null;
+        Form form = repo.findByTitle(title).get();
+       return new ResponseDto<>(true,"Successfully found with title",form);
     }
 }
