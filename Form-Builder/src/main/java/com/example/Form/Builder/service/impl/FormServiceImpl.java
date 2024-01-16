e1e5b8cb142461c241167f380d9ef028eb09917d
abhijithjana-001 <abhijith.kme19cs002@gmail.com>
2024-01-16 11:36:20 +0530

    sql form service

--- Form-Builder/src/main/java/com/example/Form/Builder/service/impl/FormServiceImpl.java
+++ Form-Builder/src/main/java/com/example/Form/Builder/service/impl/FormServiceImpl.java
@@ -1,72 +1,72 @@
-package com.example.Form.Builder.service.impl;
-
-import com.example.Form.Builder.config.MongoFormMapStruct;
-import com.example.Form.Builder.dto.response.ResponseDto;
-
-import com.example.Form.Builder.entities.entity.Form;
-
-import com.example.Form.Builder.repostory.SqlRepo;
-import com.example.Form.Builder.repostory.mongodb.repo.MongoRepo;
-
-import com.example.Form.Builder.service.FormService;
-import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
-import java.util.List;
-
-@Service
-public class FormServiceImpl implements FormService {
-    @Value("${current.database}")
-    private String dbName;
-
-
-
-
-
-
-    public ResponseDto<List<Object>> saveOrUpdateForm(Form form, String title){
-
-
-      if(title==null){
-
-
-          return new ResponseDto<>(true,"Form created successfully ", Arrays.asList( ));
-      }
-      else{
-//          Form form =
-//          form.setId(form1.getId());
-//          Form save = formRepo.save(form);
+//package com.example.Form.Builder.service.impl;
 //
-//          Form form2 = postgreRepo.findByTitle(title).get();
-//          form.setId(form2.getId());
-//          Form save2= postgreRepo.save(form);
-
-//          MongoForm form3 = mongoRepo.findByTitle(title).get();
-//          MongoForm mongoForm=mongoFormMapStruct.toEntity(form);
-//          mongoForm.set_id(form3.get_id());
-//         MongoForm save3= mongoRepo.save(mongoForm);
-
-
-          return new ResponseDto<>(true,"Form updated successfully",Arrays.asList());
-      }
-    }
-
-
-    public ResponseDto<Form> deleteFormByTitle(String title){
-//        Form form1 = formRepo.findByTitle(title).get();
-//      formRepo.deleteById(form1.getId());
-//        Form form2 = postgreRepo.findByTitle(title).get();
-//        postgreRepo.deleteById(form2.getId());
-//        mongoRepo.deleteByTitle(title);
-
-        return new ResponseDto<>(true,"Form deleted successfully",null);
-    }
-
-    public ResponseDto<Form> getFormByTitle(String title){
-//        Form form = formRepo.findByTitle(title).get();
-        return new ResponseDto<>(true,"Successfully found with title",null);
-    }
-}
+//import com.example.Form.Builder.config.MongoFormMapStruct;
+//import com.example.Form.Builder.dto.response.ResponseDto;
+//
+//import com.example.Form.Builder.entities.entity.Form;
+//
+//import com.example.Form.Builder.repostory.SqlRepo;
+//import com.example.Form.Builder.repostory.mongodb.repo.MongoRepo;
+//
+//import com.example.Form.Builder.service.FormService;
+//import lombok.val;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Service
+//public class FormServiceImpl implements FormService {
+//    @Value("${current.database}")
+//    private String dbName;
+//
+//
+//
+//
+//
+//
+//    public ResponseDto<List<Object>> saveOrUpdateForm(Form form, String title){
+//
+//
+//      if(title==null){
+//
+//
+//          return new ResponseDto<>(true,"Form created successfully ", Arrays.asList( ));
+//      }
+//      else{
+////          Form form =
+////          form.setId(form1.getId());
+////          Form save = formRepo.save(form);
+////
+////          Form form2 = postgreRepo.findByTitle(title).get();
+////          form.setId(form2.getId());
+////          Form save2= postgreRepo.save(form);
+//
+////          MongoForm form3 = mongoRepo.findByTitle(title).get();
+////          MongoForm mongoForm=mongoFormMapStruct.toEntity(form);
+////          mongoForm.set_id(form3.get_id());
+////         MongoForm save3= mongoRepo.save(mongoForm);
+//
+//
+//          return new ResponseDto<>(true,"Form updated successfully",Arrays.asList());
+//      }
+//    }
+//
+//
+//    public ResponseDto<Form> deleteFormByTitle(String title){
+////        Form form1 = formRepo.findByTitle(title).get();
+////      formRepo.deleteById(form1.getId());
+////        Form form2 = postgreRepo.findByTitle(title).get();
+////        postgreRepo.deleteById(form2.getId());
+////        mongoRepo.deleteByTitle(title);
+//
+//        return new ResponseDto<>(true,"Form deleted successfully",null);
+//    }
+//
+//    public ResponseDto<Form> getFormByTitle(String title){
+////        Form form = formRepo.findByTitle(title).get();
+//        return new ResponseDto<>(true,"Successfully found with title",null);
+//    }
+//}
--- Form-Builder/src/main/java/com/example/Form/Builder/service/impl/SqlFormService.java
+++ Form-Builder/src/main/java/com/example/Form/Builder/service/impl/SqlFormService.java
@@ -1,4 +1,44 @@
 package com.example.Form.Builder.service.impl;
 
-public class SqlFormService {
+import com.example.Form.Builder.dto.response.ResponseDto;
+import com.example.Form.Builder.entities.entity.Form;
+import com.example.Form.Builder.repostory.SqlRepo;
+import com.example.Form.Builder.service.FormService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+
+public class SqlFormService implements FormService {
+
+    @Autowired
+    private SqlRepo repo;
+
+    @Override
+    public ResponseDto<List<Object>> saveOrUpdateForm(Form form, String title) {
+
+        if(title!=null){
+          form.setId(repo.findByTitle(title).get().getId());
+
+
+        }
+        Form save = repo.save(form);
+
+        return new ResponseDto<>(true,"Form updated successfully",Arrays.asList(save));
+
+    }
+
+    @Override
+    public ResponseDto<Form> deleteFormByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<Form> getFormByTitle(String title) {
+        return null;
+    }
 }
