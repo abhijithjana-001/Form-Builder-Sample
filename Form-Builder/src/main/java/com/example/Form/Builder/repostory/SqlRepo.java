52981c2940349e9965cf36022b458d1e9f572548
abhijithjana-001 <abhijith.kme19cs002@gmail.com>
2024-01-16 11:19:53 +0530

    .

---
+++ Form-Builder/src/main/java/com/example/Form/Builder/repostory/SqlRepo.java
@@ -0,0 +1,13 @@
+package com.example.Form.Builder.repostory;
+
+import com.example.Form.Builder.entities.entity.Form;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SqlRepo extends JpaRepository<Form,Long>  {
+    void  deleteByTitle(String title);
+
+
+    Optional<Form> findByTitle(String title);
+}
---
+++ Form-Builder/src/main/java/com/example/Form/Builder/repostory/mongodb/repo/MongoConfig.java
@@ -0,0 +1,30 @@
+package com.example.Form.Builder.repostory.mongodb.repo;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@ConditionalOnProperty(name = "current.database", havingValue = "mongo")
+@EnableMongoRepositories(basePackages = {"com.example.Form.Builder.repostory.mongodb.repo"})
+public class MongoConfig {
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(environment.getProperty("spring.data.mongodb.uri"));
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), environment.getProperty("spring.data.mongodb.database"));
+    }
+}
+
